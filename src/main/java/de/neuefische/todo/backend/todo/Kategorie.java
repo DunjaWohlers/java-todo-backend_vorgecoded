6b323dc07df74fd4d8ef698c228e0e058f44a019
dunjawohlers <dunjawohlers@gmx.de>
2022-07-25 16:34:55 +0200

    vorgecodete Aufgabe, nur TodoItem:  Kategorie hinzugefügt

---
+++ src/main/java/de/neuefische/todo/backend/todo/Kategorie.java
@@ -0,0 +1,9 @@
+package de.neuefische.todo.backend.todo;
+
+public enum Kategorie {
+    EINKAUF,
+    HAUSHALT,
+    BERUF,
+    BUERO,
+    GARTEN
+}
--- src/main/java/de/neuefische/todo/backend/todo/Todo.java
+++ src/main/java/de/neuefische/todo/backend/todo/Todo.java
@@ -3,18 +3,20 @@ package de.neuefische.todo.backend.todo;
 public record Todo(
         String id,
         String description,
-        TodoStatus status
+        TodoStatus status,
+        Kategorie kategorie
 ) {
 
     Todo(
             String description,
-            TodoStatus status
+            TodoStatus status,
+            Kategorie kategorie
     ) {
-        this(null, description, status);
+        this(null, description, status,kategorie);
     }
 
 
     public Todo withId(String id) {
-        return new Todo(id, description, status);
+        return new Todo(id, description, status,kategorie);
     }
 }
